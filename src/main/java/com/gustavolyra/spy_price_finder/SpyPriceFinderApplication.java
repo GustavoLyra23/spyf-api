36e79e279cce0db8d5e54282d8b7d817067a7862
GustavoLyra23 <gustavolyra23@gmail.com>
2024-09-30 11:35:22 -0300

    first commit

---
+++ src/main/java/com/gustavolyra/spy_price_finder/SpyPriceFinderApplication.java
@@ -0,0 +1,13 @@
+package com.gustavolyra.spy_price_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpyPriceFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpyPriceFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavolyra/spy_price_finder/SpyPriceFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavolyra.spy_price_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpyPriceFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
